86d8c251f34513a4299646c0000a013976dc1ea0
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-07-12 18:16:28 -0500

    feat(coap-client): add copyright notice and fix styling

--- examples/coap/coap-client/coap-client.c
+++ examples/coap/coap-client/coap-client.c
@@ -1,3 +1,42 @@
+/*
+ * Copyright (c) 2022, Universidad de Colima, Tecnologico Nacional de Mexico
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+/**
+ * \file
+ *         This is a simple CoAP client that connects to a pre-defined server
+ *
+ * \author
+ *         Ismael Amezcua Valdovinos <ismaelamezcua@ucol.mx>
+ *         Patricia Elizabeth Figueroa Millan <patricia.figueroa@colima.tecnm.mx>
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -21,15 +60,14 @@ static struct etimer et;
 char *service_urls[1] = { ".well-known/core" };
 
 /*
- * This function will be passed to COAP_BLOCKING_REQUEST()
- * to handle responses
+ * This function will be passed to COAP_BLOCKING_REQUEST() to handle responses
  */
 void
 client_chunk_handler(coap_message_t *response)
 {
   const uint8_t *chunk;
 
-  if (response == NULL) {
+  if(response == NULL) {
     puts("Request timed out.");
     return;
   }
@@ -38,33 +76,32 @@ client_chunk_handler(coap_message_t *response)
 
   printf("|%.*s", len, (char *)chunk);
 }
-
 PROCESS_THREAD(coap_client, ev, data)
 {
   static coap_endpoint_t server_ep;
 
   PROCESS_BEGIN();
 
-  // This way the packet can be treated as a pointer
+  /* This way the packet can be treated as a pointer */
   static coap_message_t request[1];
 
-  coap_endpoint_parse(COAP_SERVER_ADDR, strlen(COAP_SERVER_ADDR), &server_ep);
+  coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
 
   etimer_set(&et, TOGGLE_INTERVAL * CLOCK_SECOND);
 
-  while (1) {
+  while(1) {
     PROCESS_YIELD();
-  
-    if (etimer_expired(&et)) {
+
+    if(etimer_expired(&et)) {
       printf("-- Toggle timer --\n");
 
-      // prepare the requet, TID is set by COAP_BLOCKING_REQUEST()
+      /* prepare the requet, TID is set by COAP_BLOCKING_REQUEST() */
       coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
       coap_set_header_uri_path(request, service_urls[1]);
 
       const char msg[] = "Toggle!";
 
-      coap_set_payload(request, (uint8_t *)msg, sizeof(msg) -1);
+      coap_set_payload(request, (uint8_t *)msg, sizeof(msg) - 1);
 
       LOG_INFO_COAP_EP(&server_ep);
       LOG_INFO_("\n");
@@ -75,7 +112,6 @@ PROCESS_THREAD(coap_client, ev, data)
 
       etimer_reset(&et);
     }
-  
   }
 
   PROCESS_END();
