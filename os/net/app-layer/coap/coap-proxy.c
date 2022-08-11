078efa012e3e41f1290dc27ea4c517979ce55f9c
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-08-11 13:06:12 -0500

    feat: handle requests and responses in the same coap_proxy_receive function

--- os/net/app-layer/coap/coap-proxy.c
+++ os/net/app-layer/coap/coap-proxy.c
@@ -55,6 +55,23 @@
 /* Volatile cache storage */
 coap_proxy_enpoints_t proxy_endpoints;
 /*---------------------------------------------------------------------------*/
+uint8_t *
+client_chunk_handler(coap_message_t *response)
+{
+  const uint8_t *chunk;
+
+  if(response == NULL) {
+    puts("Request timed out");
+    return;
+  }
+
+  int length = coap_get_payload(response, &chunk);
+
+  printf("|%.*s", length, (char *)chunk);
+
+  return chunk;
+}
+/*---------------------------------------------------------------------------*/
 int
 coap_proxy_receive(const coap_endpoint_t *src,
                    uint8_t *payload, uint16_t payload_length)
@@ -85,9 +102,28 @@ coap_proxy_receive(const coap_endpoint_t *src,
      * create a new request to the proxy_uri. If not, we MUST assume
      * the packet is a response from the target proxied node, so we
      * should gather its payload and create a response to the client.
+     * We also have to differentiate between requests and repsonses
+     * by analizying the message type and code === 0.
      */
+    if(message->code >= COAP_GET && message->code <= COAP_DELETE) {
+      /* Handle proxy requests */
+      static coap_message_t proxy_request[1];
+
+      coap_init_message(proxy_request, COAP_TYPE_CON, COAP_GET, 0);
+      coap_set_header_uri_path(proxy_request, message->proxy_uri);
+
+      LOG_DBG("CoAP Proxy: Sent a request to target client ");
+      LOG_DBG_COAP_STRING(message->proxy_uri, message->proxy_uri_len);
+      LOG_DBG_("\n");
+    } else {
+      /* Handle proxy responses */
+      LOG_DBG("Receive a response to the proxy: ");
+    }
+
     if(message->proxy_uri_len > 0) {
-      LOG_DBG("We must process the packet.");
+      uint16_t mid = random_rand();
+      coap_init_message(request, COAP_TYPE_CON, COAP_GET, mid);
+      coap_set_header_uri_path(request, message->proxy_uri);
     } else {
       LOG_DBG("We must process differently this packet.");
     }
