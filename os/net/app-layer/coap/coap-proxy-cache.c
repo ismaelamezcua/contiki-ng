5c5ef44c954c6be12d2be48715582857ad8b7409
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-09-08 06:05:44 -0500

    feat: add proxy cache entry list

---
+++ os/net/app-layer/coap/coap-proxy-cache.c
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2022, Universidad de Colima, Tecnologico Nacional de Mexico
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ */
+
+/**
+ * \file
+ *      CoAP proxy cache implementation.
+ * \author
+ *      Ismael Amezcua Valdovinos <ismaelamezcua@ucol.mx>
+ *      Patricia Elizabeth Figueroa Millan <patricia.figueroa@colima.tecnm.mx>
+ */
+
+/**
+ * \addtogroup coap-proxy
+ * @{
+ */
+
+#include "coap-engine.h"
+#include "coap-proxy-cache.h"
+#include "lib/memb.h"
+#include "lib/list.h"
+
+/* Log configuration */
+#include "coap-log.h"
+#define LOG_MODULE "coap-proxy"
+#define LOG_LEVEL  LOG_LEVEL_COAP
+
+/*---------------------------------------------------------------------------*/
+MEMB(cache_memb, coap_proxy_cache_entry_t, COAP_MAX_OPEN_TRANSACTIONS);
+LIST(cache_list);
+/*---------------------------------------------------------------------------*/
+void
+coap_proxy_new_cache_entry(char proxy_uri[],
+                           uint8_t *payload,
+                           uint16_t payload_len)
+{
+  coap_proxy_cache_entry_t *entry = memb_alloc(&cache_memb);
+
+  if(entry) {
+    memcpy(entry->proxy_uri, proxy_uri, strlen(proxy_uri));
+   // strcpy(entry->proxy_uri, proxy_uri);
+    memcpy(entry->payload, payload, (size_t)payload_len);
+    // entry->payload = payload;
+    entry->payload_len = payload_len;
+
+    LOG_DBG("Created a cache entry for URI %s: %p\n", entry->proxy_uri, entry);
+    LOG_DBG("PAYLOAD");
+    LOG_DBG_COAP_STRING((char *)entry->payload, entry->payload_len);
+    list_add(cache_list, entry);
+  }
+}
+/*---------------------------------------------------------------------------*/
+void
+coap_proxy_clear_cache_entry(coap_proxy_cache_entry_t *entry)
+{
+  if(entry) {
+    LOG_DBG("Freeing cache entry: %p\n", entry);
+    list_remove(cache_list, entry);
+    memb_free(&cache_memb, entry);
+  }
+}
+/*---------------------------------------------------------------------------*/
+coap_proxy_cache_entry_t *
+coap_proxy_get_cache_by_uri(char proxy_uri[])
+{
+  coap_proxy_cache_entry_t *entry = NULL;
+
+  for(entry = (coap_proxy_cache_entry_t *)list_head(cache_list); entry; entry = entry->next) {
+    if((strcmp(entry->proxy_uri, proxy_uri)) == 0) {
+      LOG_DBG("Found cache entry for %s: %p\n", proxy_uri, entry);
+      return entry;
+    }
+  }
+
+  return NULL;
+}
+/** @}*/
---
+++ os/net/app-layer/coap/coap-proxy-cache.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2022, Universidad de Colima, Tecnologico Nacional de Mexico
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ */
+
+/**
+ * \file
+ *      CoAP proxy cache implementation header file.
+ * \author
+ *      Ismael Amezcua Valdovinos <ismaelamezcua@ucol.mx>
+ *      Patricia Elizabeth Figueroa Millan <patricia.figueroa@colima.tecnm.mx>
+ */
+
+/**
+ * \addtogroup coap-proxy
+ * @{
+ */
+
+#ifndef COAP_PROXY_CACHE_H_
+#define COAP_PROXY_CACHE_H_
+
+#include "coap-engine.h"
+
+/* Container for a CoAP cache entry */
+typedef struct coap_proxy_cache_entry {
+  struct coap_proxy_cache_entry *next;
+
+  char proxy_uri[128];
+  uint8_t *payload;
+  uint16_t payload_len;
+} coap_proxy_cache_entry_t;
+
+void
+coap_proxy_new_cache_entry(char proxy_uri[],
+                           uint8_t *payload,
+                           uint16_t payload_len);
+
+void
+coap_proxy_clear_cache_entry(coap_proxy_cache_entry_t *entry);
+
+coap_proxy_cache_entry_t *
+coap_proxy_get_cache_by_uri(char proxy_uri[]);
+
+#endif /* COAP_PROXY_CACHE_H_ */
+/** @} */
