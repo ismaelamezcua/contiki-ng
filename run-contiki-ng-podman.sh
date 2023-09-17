faa60bbb42c8b4bb7328f5c67955cfd6364cff2f
Ismael Amezcua <amezcua.ismael@gmail.com>
2023-09-17 08:57:17 -0600

    feat: add new versioned run script and remove old non-working scripts

--- run-contiki-ng-podman.sh
+++
@@ -1,2 +0,0 @@
-CNG_PATH=$(pwd)
-podman run --privileged --sysctl net.ipv6.conf.all.disable_ipv6=0 --mount type=bind,source=$CNG_PATH,destination=/home/user/contiki-ng -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/bus/usb:/dev/bus/usb -ti contiker/contiki-ng
